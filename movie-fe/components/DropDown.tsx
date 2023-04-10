07a57a870fbc16732efcd3243b4f8e53158e8964
Morning <morning@1-9.local>
2023-04-10 09:30:32 +0800

    filter navbar init

--- movie-fe/components/DropDown.tsx
+++ movie-fe/components/DropDown.tsx
@@ -4,6 +4,6 @@
 // }
 
 export default function DropDown() {
-  const baseClass = `hidden group-hover:block absolute right-0 top-[54px] w-full h-36 border-solid rounded-lg border-2 border-white bg-white z-50 container`;
+  const baseClass = `hidden group-hover:block absolute left-0 right-0 top-[54px] w-full h-36 border-solid rounded-lg border-2 border-white bg-white z-50 container`;
   return <div className={baseClass}>Hello dropdown</div>;
 }
---
+++ movie-fe/components/Filter.tsx
@@ -0,0 +1,11 @@
+export default function Filters(): JSX.Element {
+  return (
+    <div>
+      <div>SORT</div>
+      <div>GENRE</div>
+      <div>RATING</div>
+      <div>AUDIENCE SCORE</div>
+      <div>TOMATOMETER</div>
+    </div>
+  );
+}
