bf995030c894a9bf4c003a0028aa895e528f9c7a
Naraa <naraa_togy@yahoo.com>
2023-04-29 23:38:25 +0800

    initiated filter modal component

--- movie-fe/components/Filter.tsx
+++ movie-fe/components/Filter.tsx
@@ -1,31 +1,35 @@
 import {useState} from "react";
 import Arrow from "../assets/Arrow";
+import Sort from "./modals/Sort";
 
 export default function Filter(): JSX.Element {
-  const [showDropDown, setShowDropDown] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const clickHandler = () => {
     console.log("clicked filter");
-    setShowDropDown(true);
+    setShowModal(true);
   };
 
   const filerButtonStyle =
     "flex gap-[5px] items-center justify-center py-1 px-4 my-3 border-2 border-solid border-[#bcbdbe] rounded-full hover:border-black";
   return (
-    <div className="flex gap-3">
-      <div className={filerButtonStyle} onClick={() => clickHandler}>
-        SORT <Arrow />{" "}
+    <>
+      <div className="flex gap-3">
+        <div className={filerButtonStyle} onClick={clickHandler}>
+          SORT <Arrow />{" "}
+        </div>
+        <div className={filerButtonStyle} onClick={clickHandler}>
+          GENRE <Arrow />
+        </div>
+        <div className={filerButtonStyle} onClick={clickHandler}>
+          RATING <Arrow />
+        </div>
+        <div className={filerButtonStyle} onClick={clickHandler}>
+          AUDIENCE SCORE <Arrow />
+        </div>
+        <div className={filerButtonStyle}>TOMATOMETER</div>
       </div>
-      <div className={filerButtonStyle}>
-        GENRE <Arrow />
-      </div>
-      <div className={filerButtonStyle}>
-        RATING <Arrow />
-      </div>
-      <div className={filerButtonStyle}>
-        AUDIENCE SCORE <Arrow />
-      </div>
-      <div className={filerButtonStyle}>TOMATOMETER</div>
-    </div>
+      {showModal && <Sort />}
+    </>
   );
 }
