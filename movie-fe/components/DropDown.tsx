6b26a24526a98c03d8af0082b2a32f21ec850d4d
Morning <morning@1-9.local>
2023-05-08 12:34:11 +0800

    fixed pull conflict

--- movie-fe/components/DropDown.tsx
+++ movie-fe/components/DropDown.tsx
@@ -1,9 +1,4 @@
-// export default function DropDown({ className }: { className: string }) {
-//   const baseClass = `hidden group-hover:block absolute w-full h-36 border-solid border-black rounded-lg border-2 bg-white`;
-//   return <div className={baseClass}>Hello dropdown</div>;
-// }
-
-export default function DropDown() {
-  const baseClass = `hidden group-hover:block absolute left-0 right-0 top-[54px] w-full h-36 border-solid rounded-lg border-2 border-white bg-white z-50 container`;
+export default function DropDown(): JSX.Element {
+  const baseClass = `hidden group-hover:block absolute left-0 top-[54px] max-w-[1100px] h-36 border-solid rounded-lg border-2 border-white bg-white z-50 container`;
   return <div className={baseClass}>Hello dropdown</div>;
 }
--- movie-fe/components/Filter.tsx
+++ movie-fe/components/Filter.tsx
@@ -1,11 +1,19 @@
+import {useState} from "react";
 import Arrow from "../assets/Arrow";
 
 export default function Filter(): JSX.Element {
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const clickHandler = () => {
+    console.log("clicked filter");
+    setShowDropDown(true);
+  };
+
   const filerButtonStyle =
-    "rounded-full flex gap-[5px] items-center justify-center py-1 px-4 my-3 border-solid border-2 border-black";
+    "flex gap-[5px] items-center justify-center py-1 px-4 my-3 border-2 border-solid border-[#bcbdbe] rounded-full hover:border-black";
   return (
-    <div className="flex gap-2 border-solid border-2 border-black">
-      <div className={filerButtonStyle}>
+    <div className="flex gap-3">
+      <div className={filerButtonStyle} onClick={() => clickHandler}>
         SORT <Arrow />{" "}
       </div>
       <div className={filerButtonStyle}>
